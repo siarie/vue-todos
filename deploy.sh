4027a48e40601c8e9c0fd7d9ab27d8f5ccfa59e1
Sri Aspari <sriaspari@gmail.com>
2019-10-23 11:34:25 +0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:siarie/vue-todos.git master:gh-pages
+
+cd -
